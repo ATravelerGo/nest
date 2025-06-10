557c9ca86f5b0c1d94ba5e4fb60dd25bd137b3d8
chen <xiaokeai1833103@163.com>
2025-06-10 18:12:16 +0800

    feat: 尝试开发了一个小小中间件

--- src/user/user.module.ts
+++ src/user/user.module.ts
@@ -1,12 +1,17 @@
-import { Module } from '@nestjs/common';
+import { MiddlewareConsumer, Module, NestModule } from '@nestjs/common';
 import { UserService } from './user.service';
 import { UserController } from './user.controller';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { User } from './entities/user.entity';
+import { LoggerMiddleWare } from '../middleware/LoggerMiddleWare';
 
 @Module({
   controllers: [UserController],
   providers: [UserService],
   imports: [TypeOrmModule.forFeature([User])],
 })
-export class UserModule {}
+export class UserModule implements NestModule {
+  configure(consumer: MiddlewareConsumer) {
+    consumer.apply(LoggerMiddleWare).forRoutes('user');
+  }
+}
