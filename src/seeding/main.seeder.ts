8d324f592d8c2f93e0816f18f622375aa626f432
chen <xiaokeai1833103@163.com>
2025-06-15 19:16:00 +0800

    seed的用啊

--- src/seeding/main.seeder.ts
+++ src/seeding/main.seeder.ts
@@ -11,31 +11,31 @@ export class MainSeeder implements Seeder {
     dataSource: DataSource,
     factoryManager: SeederFactoryManager,
   ): Promise<any> {
+    // 创建propertyTypes mock数据 并保存到数据库
     const typeRepo = dataSource.getRepository(PropertyType);
     const propertyTypes = await typeRepo.save([
       { value: 'Condo' },
       { value: 'Apartment' },
     ]);
 
+    // 创建users mock数据 并保存到数据库
     const userFactory = factoryManager.get(User);
-    const users = await userFactory.saveMany(10);
+    const users = await userFactory.saveMany(10); //这个其实就是保存到数据了也
 
+    const propertyRepo = dataSource.getRepository(Property);
     const propertyFeatureFactory = factoryManager.get(PropertyFeature);
-
     const propertyFactory = factoryManager.get(Property);
     const properties = await Promise.all(
       Array(50)
         .fill('')
         .map(async () => {
-          return await propertyFactory.make({
+          return propertyFactory.make({
             user: faker.helpers.arrayElement(users),
             type: faker.helpers.arrayElement(propertyTypes),
             propertyFeature: await propertyFeatureFactory.save(),
           });
         }),
     );
-
-    const propertyRepo = dataSource.getRepository(Property);
     await propertyRepo.save(properties);
   }
 }
--- src/seeding/seed.ts
+++ src/seeding/seed.ts
@@ -13,10 +13,21 @@ const options: DataSourceOptions & SeederOptions = {
   seeds: [MainSeeder],
 };
 
-const dataSource = new DataSource(options);
+/*
+* 因为 seed 脚本通常是独立于应用启动的程序，它不是 NestJS 的模块，也不是 HTTP 请求生命周期的一部分。
+所以你需要自己手动：
+	•	创建数据库连接（new DataSource）
+	•	初始化连接（dataSource.initialize()）
+	•	通过连接执行插入操作（比如用工厂或 Seeder）
+这一步是必需的，否则你没法操作数据库。
+*
+*
+* */
+
+const dataSource = new DataSource(options); //与当前数据库建立关系
 
 dataSource.initialize().then(async () => {
-  await dataSource.synchronize(true);
-  await runSeeders(dataSource);
+  await dataSource.synchronize(true); //它会检查你的实体类定义（比如字段、类型、关系），并自动同步到数据库中对应的表结构。   true 参数表示强制先清空数据库里的表再重新建，相当于“重置数据库”。
+  await runSeeders(dataSource); //运行所有你配置好的 Seeder（种子脚本）。
   process.exit();
 });
